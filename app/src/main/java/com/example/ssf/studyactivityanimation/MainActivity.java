0188f2f847af46ee524874a6fd029ee71eea3ed9
H-Destiny <2026652784@qq.com>
2017-02-09 16:22:28 +0800

    over

--- app/src/main/java/com/example/ssf/studyactivityanimation/MainActivity.java
+++ app/src/main/java/com/example/ssf/studyactivityanimation/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.ssf.studyactivityanimation;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//定时的线程
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent it = new Intent(MainActivity.this,
+                        AppActivity.class);
+                startActivity(it);
+                finish();
+            }
+        },1000);
     }
 }
